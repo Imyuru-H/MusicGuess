e32a14e9968aaf331350b7c50ed0a57d3c67ba73
Imyuru_ <liusibo986@163.com>
2025-05-11 22:38:41 +0800

    add bpm crawler to the crawler module.

--- crawler.py
+++ crawler.py
@@ -13,18 +13,19 @@ __author__ = 'Imyuru_'
 
 # Copyright (c) 2025 Imyuru_. Licensed under MIT License.
 
-import os, sys, time, random
+import os, re
 os.environ["PYTHONIOENCODING"] = "utf-8"
 
 import requests
+import bs4
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 from urllib.parse import urljoin
 
 
 class Config:
-    BASE_URL = "https://phigros.fandom.com/"
-    START_URL = "https://phigros.fandom.com/wiki/"
+    START_URL_MOE = r"https://mzh.moegirl.org.cn/Phigros/%E6%9B%B2%E7%9B%AE%E5%88%97%E8%A1%A8"
+    START_URL_WIKI = r"https://phigros.fandom.com/wiki/"
     HEADERS = {
         "User-Agent": UserAgent().random,
         "Accept-Language": "en-US,en;q=0.9",
@@ -57,6 +58,10 @@ class Crawler:
         """Parse html pages 解析详情页"""
         soup = BeautifulSoup(html.text, "lxml")
         return soup
+
+class WikiCrawler(Crawler):
+    def __init__(self):
+        super().__init__()
     
     def _get_song_data(self, html:BeautifulSoup) -> str:
         table = html.find('table')
@@ -73,6 +78,7 @@ class Crawler:
             'pack' : tb_lst[tb_lst.index('Pack') + 1],
             'difficulty' : tb_lst[tb_lst.index('Difficulty') + 1 : tb_lst.index('Level')],
             'level' : tb_lst[tb_lst.index('Pack') + 1 : tb_lst.index('Note count')],
+            'note count' : tb_lst[tb_lst.index('Note count') + 1 : tb_lst.index('Artist')],
             'artist' : tb_lst[tb_lst.index('Artist') + 1],
             'illustration' : tb_lst[tb_lst.index('Illustration') + 1],
             'duration' : tb_lst[tb_lst.index('Duration') + 1],
@@ -87,16 +93,88 @@ class Crawler:
     
     def run(self, song:str) -> dict:
         song = song.replace(' ','_')
-        resp = self._fetch(f"{Config.START_URL}{song}")
+        resp = self._fetch(f"{Config.START_URL_WIKI}{song}")
         data = self._parse_html(resp)
         table = self._get_song_data(data)
         info = self._parse_table_lst(table)
         
         return info
 
+class MoeCrawler(Crawler):
+    def __init__(self):
+        super().__init__()
+    
+    def _get_n_parse_song_data(self, html:BeautifulSoup) -> dict:
+        def get_content(text:str) -> str:
+            cleaned_text = re.sub(r'<.*?>', '', text)
+            return cleaned_text
+
+        def lst_split(lst:list, step:int) -> list[list]:
+            func = lambda a,b : map(lambda c:a[c:c+b],range(0,len(a),b))
+            res = list(func(lst, step))
+            return res
+
+        def is_valid_format(s):
+            pattern = r'^\d+\s+\(\d+\.\d+\)$'
+            if re.match(pattern, s):
+                return True
+            else:
+                return False
+        
+        table = html.find_all('table')
+
+        tb_dict = {
+            'main story' : table[1].find_all('td'),
+            'side story' : table[2].find_all('td'),
+            'ex chapter' : table[3].find_all('td'),
+            'ex story' : table[4].find_all('td'),
+        }
+        
+        tmp = []
+        for key in tb_dict:
+            for elm in tb_dict[key]:
+                tmp.append(get_content(str(elm)))
+            tb_dict[key] = tmp
+            tmp = []
+        
+        for key in tb_dict:
+            tb_dict[key] = [item for item in tb_dict[key] if "\n" not in item and not is_valid_format(item)]
+        
+        for key in tb_dict:
+            tb_dict[key] = lst_split(tb_dict[key],3)
+        
+        for key in tb_dict:
+            for lst in tb_dict[key]:
+                tb_dict[key][tb_dict[key].index(lst)] = [lst[0],lst[2]]
+        
+        bpm_dict = {}
+        for key in tb_dict:
+            for lst in tb_dict[key]:
+                bpm_dict[lst[0]] = lst[1]
+        
+        return bpm_dict
+    
+    def run(self, song:str) -> str:
+        resp = self._fetch(Config.START_URL_MOE)
+        data = self._parse_html(resp)
+        bpm_dict = self._get_n_parse_song_data(data)
+        bpm = bpm_dict[song]
+        
+        return bpm
+
+class PhiCrawler:
+    def run(self, song:str) -> dict:
+        info_crawler = WikiCrawler()
+        bpm_crawler = MoeCrawler()
+        
+        info = info_crawler.run(song)
+        info['bpm'] = bpm_crawler.run(song)
+        
+        return info
+
 
 if __name__ == "__main__":
-    crawler = Crawler()
-    info = crawler.run('Re:End of a Dream')
+    crawler = PhiCrawler()
+    info = crawler.run('S.A.T.E.L.L.I.T.E.')
     print(info)
     
