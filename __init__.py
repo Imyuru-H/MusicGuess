d09f561eb8da8dcaa7b5dc483863716b1c22cef9
Imyuru_ <liusibo986@163.com>
2025-05-11 18:17:20 +0800

    changes venv enviorment into UV.

--- __init__.py
+++ __init__.py
@@ -11,6 +11,6 @@ import os, platform
 _path = ".\\.venv\\Scripts\\"
 
 if platform.python_version().split(".")[1] > '11':
-    os.system(f"{_path}pip.exe install setuptools")
+    os.system(f"uv pip install setuptools")
 
-os.system(f"{_path}pip.exe install -r requirements.txt")
+os.system(f"uv pip install -r requirements.txt")
